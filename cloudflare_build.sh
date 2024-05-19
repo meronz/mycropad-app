6762b066f5a00060aea5a67e52139717c69f9e3f
Salvatore Merone <sal.merone95@gmail.com>
2024-05-19 13:01:00 +0200

    fix build

--- cloudflare_build.sh
+++ cloudflare_build.sh
@@ -3,4 +3,9 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
+curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-amd64
+chmod +x tailwindcss-linux-amd64
+mkdir -p /tmp/bin
+mv tailwindcss-linux-amd64 /tmp/bin/tailwindcss
+export PATH=$PATH:/tmp/bin
 ./dotnet/dotnet publish -c Release -o output
